b5e1e311d2308f213d3955dbbe360c1ff85bf4bd
Anthony <anthonyvovo123@gmail.com>
2022-05-25 12:29:48 -0700

    fixed issue where click to one square update values in other squares

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ function Game(){
   let player1 = new Player('X');
   let player2 = new Player('O');
   let currentPlayer = player1;
+
+  // console.log(board);
   
   const TURNS = 9;
   
@@ -14,12 +16,13 @@ function Game(){
   }
 
   function clickHandler(event, player = currentPlayer){
+    console.log('\ncurrent player: ' + player.marker);
     let squareId = event.target.id;
-
+    console.log('square chosen: ' + squareId);
     board.registerChoice(squareId, player.marker);
 
     currentPlayer = currentPlayer === player1 ? player2 : player1;
-    console.log(currentPlayer);
+    console.log('current player switched, new current player: ' + currentPlayer.marker);
   }
 
   return { play };
@@ -32,8 +35,7 @@ function Player(marker){
 function Board(cbClickHandler){
   const container = document.getElementById('board-container');
 
-  let board = new Array(9);
-  board.fill(Square());
+  let board = [Square(), Square(), Square(), Square(), Square(), Square(), Square(), Square(), Square()];
 
   function display(){
     board.forEach((square, id) => {
@@ -50,27 +52,24 @@ function Board(cbClickHandler){
   }
 
   function registerChoice(squareId, marker) {
+    console.log(`square  chosen: ${squareId}, marker: ${marker}`);
+
     if (board[squareId].isFilled()) return;
 
     board[squareId].value = marker;
     document.getElementById(squareId).innerText = marker;
+    console.log(board);
   }
 
   return {board, display, registerChoice};
 }
 
 function Square(){
-  let square = { value: undefined };
-
-  function isFilled() {
-    return square.value !== undefined;
-  }
-
-  function update(newValue) {
-    square.value = newValue;
-  }
-
-  return { square, isFilled, update };
+  return { 
+    value: undefined,
+    isFilled() { return this.value !== undefined },
+    update(newValue) { this.value = newValue },
+  };
 }
 
 let game = Game();
